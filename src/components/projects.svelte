7eb0d02edb37b94cb378ef2b9856e8b69e27efb5
Thiago Amendola <amendolathiago@gmail.com>
2023-11-23 13:00:08 -0800

    Create Project component

---
+++ src/components/projects.svelte
@@ -0,0 +1,5 @@
+<script>
+    let katazuke = "/images/katazuke.jpeg";
+</script>
+
+<img src={katazuke} alt="Screenshot of game Katazuke">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script lang="ts">
+  import Projects from "../components/projects.svelte"
+</script>
+
 <h1>Thiago Amendola Page</h1>
 <p>This is Thiago Amendola's Webpage</p>
+
+<div class="wrapper-content">
+
+  <Projects />
+
+</div>
+
+<style>
+
+</style>
