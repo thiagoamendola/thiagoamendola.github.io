853405f5b999ccb3470e86beac3d4f75febe1527
Thiago Amendola <amendolathiago@gmail.com>
2023-11-24 10:40:10 -0800

    Add Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,16 @@
   import Projects from "../components/projects.svelte"
 </script>
 
-<h1>Thiago Amendola Page</h1>
-<p>This is Thiago Amendola's Webpageeee</p>
+<h1 text-lg font-bold underline>
+    Thiago Amendola Page
+</h1>
+<p>
+    This is Thiago Amendola's Webpage
+</p>
+
+<button class="h-10 px-6 font-semibold rounded-md border bg-blue-600 border-slate-950 text-slate-50" type="button">
+    I'm a test button
+</button>
 
 <div class="wrapper-content">
 
