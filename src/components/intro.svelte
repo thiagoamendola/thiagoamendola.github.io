daa744a26bf10ee82ff8a8b6f3296720770d95da
Thiago Amendola <amendolathiago@gmail.com>
2023-11-24 12:01:02 -0800

    Create first span

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -1,5 +1,13 @@
-<div>
-    <p>
-        Hello! My name is <b>Thiago Amendola</b>. My objective as a Game Developer is to create enjoyable, meaningful and impactful experiences. 
-    </p>
+<div class="relative isolate px-6 pt-14 lg:px-8 bg-slate-400">
+	<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+		<p>
+			Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 6 years
+			of industry. My specialties are
+			<span
+				class="inline-flex bg-rose-900 text-white rounded-md items-center px-1.5 py-1 text-sm font-medium ring-inset"
+				>Gameplay</span
+			>, Procedural Generation and UI. My objective as a Game Developer is to create enjoyable,
+			meaningful and impactful experiences.
+		</p>
+	</div>
 </div>
