32232a9198cd6d1ee69be3b781f46dd70bcf882d
Thiago Amendola <amendolathiago@gmail.com>
2023-11-25 15:34:55 -0800

    Fix opening of navbar

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,3 +1,17 @@
+<script>
+	let isMenuOpen = false;
+
+	const handleMenuButtonClick = () => {
+		isMenuOpen = !isMenuOpen;
+	};
+
+	const handleMenuFocusLoss = ({ relatedTarget, currentTarget }) => {
+		// use "focusout" event to ensure that we can close the dropdown when clicking outside or when we leave the dropdown with the "Tab" button
+		if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return; // check if the new focus target doesn't present in the dropdown tree (exclude ul\li padding area because relatedTarget, in this case, will be null)
+		isMenuOpen = false;
+	};
+</script>
+
 <div class="fixed z-10 w-full">
 	<nav class="flex items-center justify-between flex-wrap bg-stone-800 p-6">
 		<div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -6,17 +20,33 @@
 		<div class="block lg:hidden">
 			<button
 				class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-slate-300 hover:border-slate-300"
+				on:click={handleMenuButtonClick}
 			>
 				<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 					><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg
 				>
 			</button>
 		</div>
-		<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+		<!-- {#if isMenuOpen} -->
+		<div
+			class="w-full block flex-grow {isMenuOpen ? '' : 'hidden'} lg:flex lg:items-center lg:w-auto"
+			on:focusout={handleMenuFocusLoss}
+		>
 			<div class="text-sm lg:flex-grow">
-				<a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4">
+				<a
+					href="#home"
+					class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4"
+				>
+					Home
+				</a>
+				<a
+					href="#personal-projects"
+					class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-slate-300 mr-4"
+				>
+					Personal Projects
 				</a>
 			</div>
 		</div>
+		<!-- {/if} -->
 	</nav>
 </div>
