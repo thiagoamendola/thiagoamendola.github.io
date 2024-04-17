1db5258b6e8d6561ccde178dbfa701a074066d99
Thiago Amendola <amendolathiago@gmail.com>
2024-04-17 00:41:06 -0700

    Fix intro text sizes

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -1,12 +1,12 @@
 <div id="home" class="relative isolate px-6 pt-14 lg:px-8 bg-slate-400">
 	<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-		<p>
+		<p class="text-lg">
 			Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 6 years
 			of industry. My specialties are
-			<span class="skill-span bg-rose-900">Gameplay</span>,
-			<span class="skill-span bg-lime-600">Procedural Generation</span> and
-			<span class="skill-span bg-cyan-600">UI</span>. My objective as a Game Developer is to create
-			enjoyable, meaningful and impactful experiences.
+			<span class="skill-span text-base bg-rose-900">Gameplay</span>,
+			<span class="skill-span text-base bg-lime-600">Procedural Generation</span> and
+			<span class="skill-span text-base bg-cyan-600">UI</span>. My objective as a Game Developer is
+			to create enjoyable, meaningful and impactful experiences.
 		</p>
 	</div>
 </div>
