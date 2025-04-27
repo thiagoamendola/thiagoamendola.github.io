1b72d26b40ea31ae710eef0b3cff871154bc234c
Thiago Amendola <amendolathiago@gmail.com>
2025-04-27 01:44:50 -0700

    Made package update fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';  // <— note the package
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
