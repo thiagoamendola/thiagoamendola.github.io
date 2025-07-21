20a3aec02984db44705a83c1c1d3ca6dfc8cbe3d
Thiago Amendola <amendolathiago@gmail.com>
2025-07-21 10:57:03 -0700

    Update experience time

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -1,7 +1,7 @@
 <div id="home" class="relative isolate px-6 pt-14 lg:px-8 bg-slate-400">
 	<div class="mx-8 md:mx-auto max-w-2xl py-48 md:py-32 lg:py-56">
 		<p class="text-lg">
-			Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 6 years
+			Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 8 years
 			of industry. My specialties are
 			<span class="skill-span text-base bg-rose-900">Gameplay</span>,
 			<span class="skill-span text-base bg-lime-600">Procedural Generation</span> and
