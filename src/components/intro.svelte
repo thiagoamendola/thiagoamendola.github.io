3d012fd6d3028fc626d660a766c69ba2cb4ee326
Thiago Amendola <amendolathiago@gmail.com>
2025-08-15 07:03:37 -0700

    Add wavey hand

--- src/components/intro.svelte
+++ src/components/intro.svelte
@@ -1,12 +1,17 @@
-<div id="home" class="relative isolate px-6 lg:px-8 bg-slate-400">
-	<div class="mx-8 md:mx-auto max-w-2xl py-14 lg:py-16">
-		<p class="text-lg">
-			Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 8 years
-			of industry. My specialties are
-			<span class="skill-span text-base bg-rose-900">Gameplay</span>,
-			<span class="skill-span text-base bg-lime-600">Procedural Generation</span> and
-			<span class="skill-span text-base bg-cyan-600">UI</span>. My objective as a Game Developer is
-			to create enjoyable, meaningful and impactful experiences.
-		</p>
+<div id="home" class="relative isolate px-6 lg:px-8 bg-[#d3dded]">
+	<div class="mx-8 md:mx-auto max-w-2xl py-24 lg:py-16">
+		<div class="flex items-center">
+			<div class="mr-3 hidden md:block md:text-9xl" aria-label="wave" title="wave">👋</div>
+			<div class="flex-1">
+				<p class="text-lg">
+					Hello! My name is <b>Thiago Amendola</b>. I'm a professional programmer with more than 8 years
+					of industry. My specialties are
+					<span class="skill-span text-base bg-rose-900">Gameplay</span>,
+					<span class="skill-span text-base bg-lime-600">Procedural Generation</span> and
+					<span class="skill-span text-base bg-cyan-600">UI</span>. My objective as a Game Developer is
+					to create enjoyable, meaningful and impactful experiences.
+				</p>
+			</div>
+		</div>
 	</div>
 </div>
