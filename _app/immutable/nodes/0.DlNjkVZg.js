7a5b3a1252c663bc67d0033d3deac8536f722028
Thiago Amendola <amendolathiago@gmail.com>
2025-08-19 20:40:48 -0700

    Updates

--- _app/immutable/nodes/0.DlNjkVZg.js
+++
@@ -1,1 +0,0 @@
-import{c as a,a as n}from"../chunks/C5AM2mcY.js";import"../chunks/uNI8LcmS.js";import{k as m}from"../chunks/CnMkLD89.js";import{s}from"../chunks/C3TTzvfV.js";const l=!0,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:l},Symbol.toStringTag,{value:"Module"}));function d(t,e){var o=a(),r=m(o);s(r,e,"default",{}),n(t,o)}export{d as component,u as universal};
